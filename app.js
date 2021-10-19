7f584f15d1f1e1671afec6b9794cb37c9f4e0c70
Remish Varsani <scspl.remish@gmail.com>
2021-10-19 09:08:14 +0530

    init project.-Remish

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const express = require('express');
 const errorHandler = require('./middleware/error_handler_middleware');
 const bodyParser = require('body-parser');
 const userRouter = require('./routers/user_route');
-const payoutModeRouter = require('./routers/payout_mode_route');
-const productRouter = require('./routers/product_route');
-const orderRouter = require('./routers/order_route');
-const swaggerUI = require("swagger-ui-express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,9 +13,6 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/user',userRouter);
-app.use('/payoutMode',payoutModeRouter);
-app.use('/product',productRouter);
-app.use('/order',orderRouter);
 
 app.use(errorHandler);
 
